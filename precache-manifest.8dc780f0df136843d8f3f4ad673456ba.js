d0d7ad2cd73ee616942c990377628a0d7f5e690c
Steven Boulom <sboulom@gmail.com>
2020-06-23 19:13:39 -0700

    Updates

---
+++ precache-manifest.8dc780f0df136843d8f3f4ad673456ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8defdd91905d8e91fa2e2af1f259e0fc",
+    "url": "/sboulom/React-Portfolio/index.html"
+  },
+  {
+    "revision": "79c1f33fbc32d2180931",
+    "url": "/sboulom/React-Portfolio/static/css/main.5d7aecdc.chunk.css"
+  },
+  {
+    "revision": "81139857c73539f9dc09",
+    "url": "/sboulom/React-Portfolio/static/js/2.12fa4b5e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sboulom/React-Portfolio/static/js/2.12fa4b5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79c1f33fbc32d2180931",
+    "url": "/sboulom/React-Portfolio/static/js/main.d2ed4a53.chunk.js"
+  },
+  {
+    "revision": "ecb28759ab8a79993283",
+    "url": "/sboulom/React-Portfolio/static/js/runtime-main.8ce8723f.js"
+  }
+]);
